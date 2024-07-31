35b02ef6ce6ba7126a467dc79d486d6c03fc6b0e
zaerbib <ilunga.joel@yahoo.fr>
2024-07-31 20:04:01 +0200

    add users features

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DataModule } from './data/data.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DataModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/data-store')],
+  imports: [DataModule, MongooseModule.forRoot('mongodb://127.0.0.1:27017/data-store'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
